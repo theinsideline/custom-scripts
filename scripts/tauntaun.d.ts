7847c39de7dfcdf8e1f8a8c13fc672149a41f9ba
morgota199 <55238829+morgota199@users.noreply.github.com>
2024-05-14 18:20:27 +0000

    Deploying to production from @ theinsideline/custom-scripts@ba8b7279deded4ef909aa845d43bb6074af0415a 🚀

--- scripts/tauntaun.d.ts
+++ scripts/tauntaun.d.ts
@@ -1,7 +1,6 @@
 export declare type BuildType = "comp" | "pro" | "frame";
 export declare type Sizes = "xs" | "s" | "m" | "l" | "xl" | "xxl";
 export declare class TawnTawn {
-    constructor();
     private _map;
     location(): void;
     myButtonBlack(): void;
