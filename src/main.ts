aecbfca6a6eb0ccf40d4b6237ff638b2a229089d
Vladimir Lysenko <55238829+morgota199@users.noreply.github.com>
2024-08-28 18:19:24 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { LeadDyno } from "./clients/leaddyno";
 import { Gtag } from "./clients/gtag";
 import { LuckyOrange } from "./clients/luckyorange";
-import { PowrIo } from "./clients/powrio";
+// import { PowrIo } from "./clients/powrio";
 import { TawnTawn } from "./scripts/tauntaun";
 
 const leadDynoClient = new LeadDyno();
 const gtagClient = new Gtag();
 const luckyOrange = new LuckyOrange();
-const powrIo = new PowrIo();
+// const powrIo = new PowrIo();
 
 leadDynoClient.init();
 gtagClient.init();
 luckyOrange.init();
-powrIo.init();
+// powrIo.init();
 
 const tauntaun = new TawnTawn();
 
